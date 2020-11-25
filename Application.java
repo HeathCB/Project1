0a496da9ca88ff7ece44708664538884180fcea1
Heath C Brown <Heath.c.b@gmail.com>
2020-11-24 22:25:40 -0600

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,25 @@
+
+public class Application {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double moneyInWallet = 43.78;
+		double itemPrice = 22.78;
+		int numberOfFriends = 8;
+		int age = 25;
+		String firstName = "Heath";
+		String lastName = "Brown";
+		char middleInitial = 'C';
+		String fullName= firstName +" "+ middleInitial +" "+lastName;
+		
+
+		System.out.println(moneyInWallet - itemPrice);
+		System.out.println(age / numberOfFriends);
+		System.out.println(fullName);
+		
+	}
+
+}
